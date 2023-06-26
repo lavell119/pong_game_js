f7d7e937af57dff6870bae487647ac38e491df47
Lee <leewhiz119@gmail.com>
2023-06-26 09:49:32 -0400

    imported code from webdevsimplified github

--- script.js
+++ script.js
@@ -1,19 +1,49 @@
-const padelLeft = document.querySelector('.paddle_left')
-const padelRight = document.querySelector('.paddle_right')
+import Ball from "./Ball.js"
+import Paddle from "./Paddle.js"
 
+const ball = new Ball(document.getElementById("ball"))
+const playerPaddle = new Paddle(document.getElementById("player-paddle"))
+const computerPaddle = new Paddle(document.getElementById("computer-paddle"))
+const playerScoreElem = document.getElementById("player-score")
+const computerScoreElem = document.getElementById("computer-score")
 
-const movePadelLeft = () => {
+let lastTime
+function update(time) {
+  if (lastTime != null) {
+    const delta = time - lastTime
+    ball.update(delta, [playerPaddle.rect(), computerPaddle.rect()])
+    computerPaddle.update(delta, ball.y)
+    const hue = parseFloat(
+      getComputedStyle(document.documentElement).getPropertyValue("--hue")
+    )
 
-    console.log(padelLeft.getBoundingClientRect().top)
-    padelLeft.getBoundingClientRect().top = padelLeft.getBoundingClientRect().top + 20
-    padelLeft.style.transform = "translateX(10px)"
-    
-    padelLeft.style.top = "10px"
+    document.documentElement.style.setProperty("--hue", hue + delta * 0.01)
 
-    
+    if (isLose()) handleLose()
+  }
+
+  lastTime = time
+  window.requestAnimationFrame(update)
 }
 
-padelLeft.addEventListener("click", movePadelLeft)
+function isLose() {
+  const rect = ball.rect()
+  return rect.right >= window.innerWidth || rect.left <= 0
+}
 
+function handleLose() {
+  const rect = ball.rect()
+  if (rect.right >= window.innerWidth) {
+    playerScoreElem.textContent = parseInt(playerScoreElem.textContent) + 1
+  } else {
+    computerScoreElem.textContent = parseInt(computerScoreElem.textContent) + 1
+  }
+  ball.reset()
+  computerPaddle.reset()
+}
 
-"translateX(10px)"
+document.addEventListener("mousemove", e => {
+  playerPaddle.position = (e.y / window.innerHeight) * 100
+})
+
+window.requestAnimationFrame(update)
